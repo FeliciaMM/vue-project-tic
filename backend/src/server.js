b3c5747ae3272c3b2823d8e680c51c898b9ed37d
Felicia <feliciamihaela2@gmail.com>
2026-01-04 17:16:42 +0200

    set up

---
+++ backend/src/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/hello', (req, res) => {
+  res.send("Hello");
+});
+
+app.listen(8000, () => {
+  console.log('server listening on port 8000');
+});
